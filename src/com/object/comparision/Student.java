b597fd827f5a3d0ea4ad11095c79c30b23297232
Rupali Choudhari <rchoudhari2407@gmail.com>
2022-07-01 00:46:58 +0530

    First Commit

---
+++ src/com/object/comparision/Student.java
@@ -0,0 +1,13 @@
+package com.object.comparision;
+
+public class Student {
+    int roll_no;
+    String  Name;
+    int Marks;
+
+    public Student(int roll_no, String name, int marks) {
+        this.roll_no = roll_no;
+        Name = name;
+        Marks = marks;
+    }
+}
